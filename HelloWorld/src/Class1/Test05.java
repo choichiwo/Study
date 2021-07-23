d16fdc76d0c13100d2e0af43f6001cf0dd887646
chiochiwo <ccu12345@naver.com>
2021-07-23 17:39:54 +0900

    8교시 커밋

--- HelloWorld/src/Class1/Test05.java
+++ HelloWorld/src/Class1/Test05.java
@@ -1,10 +1,24 @@
 package Class1;
 
+import java.util.Scanner;
+
 public class Test05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		String n= s.nextLine();
+		while(!n.equals("x")) {
+			switch(n) {
+			case "m":
+				System.out.println("메뉴리스트"); break;
+			case "o":
+				System.out.println("주문"); break;
+			case "s":
+				System.out.println("매출현황"); break;	
+			}
+			n=s.nextLine();
+		}
+		System.out.println("프로그램 종료");
 	}
 
 }
