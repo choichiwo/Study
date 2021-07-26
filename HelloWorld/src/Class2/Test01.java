a381df890181546b6f00a10bd6cd97442ff2f628
chiochiwo <ccu12345@naver.com>
2021-07-26 11:08:12 +0900

    2교시 커밋

---
+++ HelloWorld/src/Class2/Test01.java
@@ -0,0 +1,13 @@
+package Class2;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		for(int n=1;n<101; n++) {
+			if(n%2 != 0)
+			System.out.println(n);
+		}
+
+	}
+
+}
---
+++ HelloWorld/src/Class2/Test02.java
@@ -0,0 +1,15 @@
+package Class2;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		int n=5;
+		System.out.println(n+"x"+1+"="+(n*1));
+		
+		for(int i=1;i<10;i++) {
+			System.out.println(n+"x"+i+"="+(n*i));
+		}
+
+	}
+
+}
