b4310bfe0f1ba960e1dd860bdf9ff3871f6110a3
chiochiwo <ccu12345@naver.com>
2021-07-26 09:40:52 +0900

    1교시 커밋

---
+++ HelloWorld/src/Class2/Test.java
@@ -0,0 +1,14 @@
+package Class2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int n=1;
+		while(n<101) {
+			System.out.println(n);
+			n++;
+		}
+		// 1부터 100사이의 홀수만 출력하는 While문
+	}
+
+}
