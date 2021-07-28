676dd395118ba3736f99532370331ef3fb5fb620
chiochiwo <ccu12345@naver.com>
2021-07-28 11:40:32 +0900

    3교시 커밋

--- HelloWorld/src/Class3/Test01.java
+++ HelloWorld/src/Class3/Test01.java
@@ -3,29 +3,32 @@ package Class3;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Test {
+public class Test01 {
 
 	public static void main(String[] args) {
-		// ArrayList/Scanner이용
-		//학생들의 수학점수를 읽어들이는 프로그램
-		//-1이 들어올때까지 ArrayList에 입력값이 채워지면서 늘어난다
-		// 1. 모든 학생의 점수를 출력
-		// 2.점수의 총합계,평균점수를 계산해서 출력
 		Scanner s = new Scanner(System.in);
 		System.out.println("점수를 입력해주세요");
 		int num = s.nextInt();
-		int sum = 0;
+		
 		ArrayList<Integer> point=new ArrayList<Integer>();
 		//while(!str.equal
 		while(num!=-1) {
 			point.add(num);
-			sum += num;
+			
 			num = s.nextInt();
 		}
+		//입력된 점수를 출력
+		for(int i=0; i<point.size();i++) {
+			System.out.println(point.get(i));
+		}
+		int sum = 0;
+		for(int j=0;j<point.size();j++) {
+			sum=sum+point.get(j);
+		}
 		s.close();
-		System.out.println("학생들 점수"+point);
 		System.out.println("학생들 총점"+sum);
 		System.out.println("학생들 평균은"+(sum/point.size()));
+
 	}
 
 }
