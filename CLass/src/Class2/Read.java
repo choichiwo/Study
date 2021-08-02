62a9214205e9f4070c980417ed68699d669df2ae
chiochiwo <ccu12345@naver.com>
2021-08-02 10:11:51 +0900

    2교시 커밋

--- CLass/src/Class2/Read.java
+++ CLass/src/Class2/Read.java
@@ -8,10 +8,10 @@ public class Read {
 		 * 1. 캡슐화
 		 * 	  클래스(객체)
 		 *   접근제한자(Access Modifier)
-		 *   public(전체공유)
+		 *   public(무제한사용/공유)
 		 *   protected(상속: 다른 페키지에 속한 자식클래스만 공유)
 		 *   default(기본값)(같은 페키지 안에서 공유)
-		 *   private(클래스 내부에서만 공유)
+		 *   private(클래스 내부에서만 공유)-클래스의 필드
 		 * 2.상속
 		 * 3.다형성
 		 *   메소드 오버로딩
