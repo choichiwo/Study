27897d0a89abe4267fc03e3e05768ad4441198ee
chiochiwo <ccu12345@naver.com>
2021-08-02 09:30:51 +0900

    1교시 커밋

---
+++ CLass/src/Class2/Read.java
@@ -0,0 +1,24 @@
+package Class2;
+
+public class Read {
+
+	public static void main(String[] args) {
+		/*
+		 * 객체지향 언어의 특징
+		 * 1. 캡슐화
+		 * 	  클래스(객체)
+		 *   접근제한자(Access Modifier)
+		 *   public(전체공유)
+		 *   protected(상속: 다른 페키지에 속한 자식클래스만 공유)
+		 *   default(기본값)(같은 페키지 안에서 공유)
+		 *   private(클래스 내부에서만 공유)
+		 * 2.상속
+		 * 3.다형성
+		 *   메소드 오버로딩
+		 *   생성자 오버로딩
+		 * 4.추상화    
+		 */
+
+	}
+
+}
---
+++ CLass/src/Class2/Reuse.java
@@ -0,0 +1,10 @@
+package Class2;
+
+public class Reuse {
+	int nNumber;
+	
+	void setnNumber(int x) {
+		this.nNumber=x;
+	}
+
+}
